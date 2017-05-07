61083a6cfe505397f4b55bcc24e6ea2e538ae1ac
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-05-07 19:04:59 +0100

    Item class set up

---
+++ app/src/main/java/example/codeclan/com/shoppingbasketcodetest/Item.java
@@ -0,0 +1,19 @@
+package example.codeclan.com.shoppingbasketcodetest;
+
+/**
+ * Created by user on 07/05/2017.
+ */
+
+public abstract class Item {
+
+    private String name;
+    private int price;
+    private int quantity;
+
+    public Item(String name, int price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+}
