d2f14662e049fdd8a6909c96fe4c65a29f74a99c
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-05-07 21:02:10 +0100

    Bagel added

---
+++ app/src/main/java/example/codeclan/com/shoppingbasketcodetest/Classes/Bagel.java
@@ -0,0 +1,13 @@
+package example.codeclan.com.shoppingbasketcodetest.Classes;
+
+/**
+ * Created by user on 07/05/2017.
+ */
+
+public class Bagel extends Item {
+
+    public Bagel(String name, int price, int quantity){
+        super(name, price, quantity);
+    }
+
+}
