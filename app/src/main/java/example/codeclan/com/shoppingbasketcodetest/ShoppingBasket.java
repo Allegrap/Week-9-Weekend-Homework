1326c16c9acdfde7834553e8bd159d62b20864b7
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-05-07 19:02:50 +0100

    ShoppingBasket Class set up

---
+++ app/src/main/java/example/codeclan/com/shoppingbasketcodetest/ShoppingBasket.java
@@ -0,0 +1,13 @@
+package example.codeclan.com.shoppingbasketcodetest;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 07/05/2017.
+ */
+
+public class ShoppingBasket {
+
+    private ArrayList<Item> items;
+
+}
