ea076e836a2d6405f973642a4aeb13ad5dd9f422
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-05-07 20:19:20 +0100

    getName added to CustomerTest

--- app/src/test/java/example/codeclan/com/shoppingbasketcodetest/CustomerTest.java
+++ app/src/test/java/example/codeclan/com/shoppingbasketcodetest/CustomerTest.java
@@ -1,8 +1,11 @@
 package example.codeclan.com.shoppingbasketcodetest;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import example.codeclan.com.shoppingbasketcodetest.Classes.Customer;
+
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Created by user on 07/05/2017.
@@ -15,6 +18,11 @@ public class CustomerTest {
     @Before
     public void before(){
         customer = new Customer("Allegra", 50, true);
+    }
+
+    @Test
+    public void getNameTest(){
+        assertEquals("Allegra", customer.getName());
     }
 
 }
